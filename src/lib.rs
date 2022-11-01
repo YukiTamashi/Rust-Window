5eb289d470f7155749557487d72ca1649c99b1c5
thiagotrentini.t@gmail.com <TickxT@users.noreply.github.com>
2022-10-31 22:45:16 -0300

    More cleanup and some documentation

--- src/lib.rs
+++ src/lib.rs
@@ -16,67 +16,63 @@ macro_rules! unsafe_impl_default_zeroed {
     };
 }
 
-const WS_OVERLAPPED: u32 = 0x00000000;
-const WS_CAPTION: u32 = 0x00C00000;
-const WS_SYSMENU: u32 = 0x00080000;
-const WS_THICKFRAME: u32 = 0x00040000;
-const WS_MINIMIZEBOX: u32 = 0x00020000;
-const WS_MAXIMIZEBOX: u32 = 0x00010000;
-const WS_OVERLAPPEDWINDOW: u32 = WS_OVERLAPPED
+pub const SW_SHOW: CInt = 5;
+pub const WS_OVERLAPPED: u32 = 0x00000000;
+pub const WS_CAPTION: u32 = 0x00C00000;
+pub const WS_SYSMENU: u32 = 0x00080000;
+pub const WS_THICKFRAME: u32 = 0x00040000;
+pub const WS_MINIMIZEBOX: u32 = 0x00020000;
+pub const WS_MAXIMIZEBOX: u32 = 0x00010000;
+pub const WS_OVERLAPPEDWINDOW: u32 = WS_OVERLAPPED
   | WS_CAPTION
   | WS_SYSMENU
   | WS_THICKFRAME
   | WS_MINIMIZEBOX
   | WS_MAXIMIZEBOX;
-const CW_USEDEFAULT: CInt = 0x80000000_u32 as CInt;
-pub const SW_SHOW: CInt = 5;
-const IDC_ARROW: LPCWStr = MakeIntResourceW(32512);
-const WM_CLOSE: u32 = 0x0010;
-const WM_DESTROY: u32 = 0x0002;
-const WM_CREATE: u32 = 0x0001;
+pub const CW_USEDEFAULT: CInt = 0x80000000_u32 as CInt;
+pub const IDC_ARROW: LPCWStr = MakeIntResourceW(32512);
+pub const WM_CLOSE: u32 = 0x0010;
+pub const WM_DESTROY: u32 = 0x0002;
+pub const WM_CREATE: u32 = 0x0001;
+pub const WM_NCCREATE: u32 = 0x0081;
+pub const WM_PAINT: u32 = 0x000F;
+pub const COLOR_WINDOW:u32 = 5;
+pub const MB_OKCANCEL: u32 = 0x00000001;
+pub const IDOK: i32 = 1;
+pub const GWLP_USERDATA: i32 = -21;
+pub const WM_SETCURSOR: u32 = 0x0020;
 
-const WM_NCCREATE: u32 = 0x0081;
-
-const WM_PAINT: u32 = 0x000F;
-const COLOR_WINDOW:u32 = 5;
-const MB_OKCANCEL: u32 = 0x00000001;
-const IDOK: i32 = 1;
-const GWLP_USERDATA: i32 = -21;
-const WM_SETCURSOR: u32 = 0x0020;
-
-
-type Atom = Word;
-type Bool = CInt;
-type CInt = i32;
-type CUInt = u32;
-type DWord = u32;
-type Handle = *mut c_void;
-type HBrush = Handle;
-type HCursor = HIcon;
-type HDc = Handle;
-type HIcon = Handle;
-type HInstance = Handle;
-type HMenu = Handle;
-type HModule = HInstance;
-type HWnd = Handle;
-type Int = CInt;
-type Long = i32;
-type LongPtr = isize;
-type LParam = LongPtr;
-type LPMsg = *mut Msg;
-type LPVoid = *mut c_void;
-type LPWStr = *mut WChar;
-type LPCWStr = *const WCharT;
-type LResult = LongPtr;
-type UInt = CUInt;
-type UIntPtr = usize;
-type ULongPtr = usize;
-type WCharT = u16;
-type WChar = WCharT;
-type WParam = UIntPtr;
-type Word = u16;
-
-type WNDProc = Option<
+pub type Atom = Word;
+pub type Bool = CInt;
+pub type CInt = i32;
+pub type CUInt = u32;
+pub type DWord = u32;
+pub type Handle = *mut c_void;
+pub type HBrush = Handle;
+pub type HCursor = HIcon;
+pub type HDc = Handle;
+pub type HIcon = Handle;
+pub type HInstance = Handle;
+pub type HMenu = Handle;
+pub type HModule = HInstance;
+pub type HWnd = Handle;
+pub type Int = CInt;
+pub type Long = i32;
+pub type LongPtr = isize;
+pub type LParam = LongPtr;
+pub type LPMsg = *mut Msg;
+pub type LPVoid = *mut c_void;
+pub type LPWStr = *mut WChar;
+pub type LPCWStr = *const WCharT;
+pub type LResult = LongPtr;
+pub type UInt = CUInt;
+pub type UIntPtr = usize;
+pub type ULongPtr = usize;
+pub type WCharT = u16;
+pub type WChar = WCharT;
+pub type WParam = UIntPtr;
+pub type Word = u16;
+pub type WNDProc = Option<
     unsafe extern "system" fn(
         hwnd: HWnd,
         Msg: UInt,
@@ -85,6 +81,36 @@ type WNDProc = Option<
     ) -> LResult,
 >;
 
+pub enum IDCursor {
+    /// Standard arrow and small hourglass
+    AppStarting = 32650,
+    /// Standard arrow
+    Arrow = 32512,
+    /// Crosshair
+    Cross = 32515,
+    /// Hand
+    Hand = 32649,
+    /// Arrow and question mark
+    Help = 32651,
+    /// I-beam
+    IBeam = 32513,
+    /// Slashed circle
+    No = 32648,
+    /// Four-pointed arrow pointing north, south, east, and west
+    SizeAll = 32646,
+    /// Double-pointed arrow pointing northeast and southwest
+    SizeNeSw = 32643,
+    /// Double-pointed arrow pointing north and south
+    SizeNS = 32645,
+    /// Double-pointed arrow pointing northwest and southeast
+    SizeNwSe = 32642,
+    /// Double-pointed arrow pointing west and east
+    SizeWE = 32644,
+    /// Vertical arrow
+    UpArrow = 32516,
+    /// Hourglass
+    Wait = 32514,
+  }
 
 #[repr(C)]
 pub struct WNDClassW {
@@ -127,6 +153,11 @@ pub struct PaintStruct{
     rgbReserved: [u8; 32],
 }
 
+impl PaintStruct{
+    pub fn rcPaint(&self) -> &Rect{
+        &self.rcPaint
+    }
+}
 #[repr(C)]
 pub struct Rect{
     left: i32,
@@ -136,7 +167,7 @@ pub struct Rect{
 }
 
 #[repr(C)]
-pub struct CREATESTRUCTW{
+pub struct CreateStructW{
     lpCreateParams: LPVoid,
     hInstance: HInstance,
     hMenu: HMenu,
@@ -149,17 +180,22 @@ pub struct CREATESTRUCTW{
     lpszName: LPCWStr,
     lpszClass: LPCWStr,
     dwExStyle: DWord,
-  }
+}
+
+impl CreateStructW{
+    pub fn lpCreateParams(&self) -> LPVoid{
+        self.lpCreateParams
+    }
+}
 
 unsafe_impl_default_zeroed!(WNDClassW);
 unsafe_impl_default_zeroed!(Msg);
 unsafe_impl_default_zeroed!(Rect);
 unsafe_impl_default_zeroed!(PaintStruct);
 
-
 #[link(name = "Kernel32")]
 extern "system" {
-    pub fn GetModuleHandleW (lpModuleName: LPCWStr) -> HModule;
+    fn GetModuleHandleW (lpModuleName: LPCWStr) -> HModule;
     pub fn GetLastError() -> DWord;
     pub fn CreateWindowExW(
         dwExStyle: DWord,
@@ -187,7 +223,7 @@ extern "system" {
     pub fn DefWindowProcW(
       hWnd: HWnd, Msg: UInt, wParam: WParam, lParam: LParam,
     ) -> LResult;
-    pub fn GetMessageW(
+    fn GetMessageW(
       lpMsg: LPMsg,
       hWnd: HWnd,
       wMsgFilterMin: UInt,
@@ -242,57 +278,20 @@ extern "system" {
     ) -> HCursor;
 }
 
+fn get_handle() -> HModule {
+    unsafe{GetModuleHandleW(std::ptr::null())}
+}
 
-pub const fn MakeIntResourceW(i: Word) -> LPWStr {
+const fn MakeIntResourceW(i: Word) -> LPWStr {
     i as ULongPtr as LPWStr
 }
 
-unsafe extern "system" fn window_procedure(hWnd: HWnd, Msg: UInt, wParam: WParam, lParam: LParam,) -> LResult{
-    match Msg{
-        WM_NCCREATE => {
-            let createstruct: *mut CREATESTRUCTW = lParam as *mut _;
-            if createstruct.is_null() {
-                return 0;
-            }
-            let boxed_i32_ptr: *mut i32 = (*createstruct).lpCreateParams.cast();
-            SetWindowLongPtrW(hWnd, GWLP_USERDATA, boxed_i32_ptr as LongPtr);
-            return 1;
-        }
-        WM_CLOSE => {let message = wide_str("Do you really want to exit?");
-            let title = wide_str("Quit");
-            let result = MessageBoxW(hWnd, message.as_ptr(), title.as_ptr(), MB_OKCANCEL);
-            if result == IDOK{
-            DestroyWindow(hWnd);}
-            else {
-                return 0;
-            }
-        },
-        WM_DESTROY => {
-            PostQuitMessage(0);
-            let ptr = GetWindowLongPtrW(hWnd, GWLP_USERDATA) as *mut i32;
-            drop(Box::from_raw(ptr));
-        },
-        WM_PAINT => {
-            let mut ps = PaintStruct::default();
-            let hdc = BeginPaint(hWnd, &mut ps);
-            FillRect(hdc, &ps.rcPaint, (COLOR_WINDOW +5) as HBrush);
-            EndPaint(hWnd, &ps);
-          }
-         _ => return DefWindowProcW(hWnd, Msg, wParam, lParam),
-        }
-        0
-}
-
-pub fn register_window(name: Vec<u16>,h_instance: *mut c_void) -> WNDClassW{
+fn register_window(name: Vec<u16>, h_instance: *mut c_void, hCursor: HCursor, procedure: unsafe extern "system" fn(HWnd, UInt, WParam, LParam) -> LResult) -> WNDClassW{
     let wc = WNDClassW{
-    lpfnWndProc: Some(window_procedure),
+    lpfnWndProc: Some(procedure),
     hInstance: h_instance,
     lpszClassName: name.as_ptr(),
-    hCursor: unsafe{
-        LoadCursorW(
-            null_mut(),
-            IDC_ARROW)
-        },
+    hCursor,
     ..Default::default()
     };
     let atom = unsafe {RegisterClassW(&wc)};
@@ -303,7 +302,7 @@ pub fn register_window(name: Vec<u16>,h_instance: *mut c_void) -> WNDClassW{
     wc
 }
 
-pub fn make_window(name: Vec<u16>,h_instance: *mut c_void) -> HWnd{
+fn make_window(name: Vec<u16>,h_instance: *mut c_void) -> HWnd{
     let lparam: *mut i32 = Box::leak(Box::new(5_i32));
     let hwnd = unsafe{CreateWindowExW(
         0,                              
@@ -325,8 +324,51 @@ pub fn make_window(name: Vec<u16>,h_instance: *mut c_void) -> HWnd{
     hwnd
 }
 
+//Handles entire creation of the window.
+//Returns window handle.
+pub fn create_window(name: Vec<u16>, hCursor: HCursor, procedure: unsafe extern "system" fn(HWnd, UInt, WParam, LParam) -> LResult) -> HWnd {
+    let hinstance = get_handle();
+    register_window(name.clone(), hinstance, hCursor, procedure);
+    let hwnd = make_window(name, hinstance);
+    unsafe {ShowWindow(hwnd, SW_SHOW)};
+    hwnd
+}
+
+//Returns cursor handle based on type called.
+pub fn cursor(name: IDCursor) -> Result<HCursor, ()> {
+    let cursor = unsafe{
+        LoadCursorW(
+            null_mut(),
+            unsafe{MakeIntResourceW(name as Word)})
+    };
+    if cursor.is_null(){
+        Err(())
+    }
+    else{
+        Ok(cursor)
+    }
+    
+}
+
+//Converts into UTF-8 string.
 pub fn wide_str(str: &str) -> Vec<u16> {
     str.encode_utf16().chain(Some(0)).collect()
 }
+
+//Calls for system message updates.
+//If it receives call to close, returns true.
+pub fn update(msg: &mut Msg) -> bool{
+    let message = unsafe{GetMessageW(msg, null_mut(), 0, 0)};
+    if message == 0{return true}
+    else if message == -1{
+        panic!();
+    }
+    else {
+        unsafe{TranslateMessage(msg); 
+        DispatchMessageW(msg);}
+    }
+    false
+}
+
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,61 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use triangle::win32::*;
-use std::ptr::{null_mut};
+
 
 fn main() {
     let name = wide_str("window");
-    let hinstance = unsafe{GetModuleHandleW(core::ptr::null())};
-    register_window(name.clone(), hinstance);
-    let hwnd = make_window(name, hinstance);
-    unsafe {ShowWindow(hwnd, SW_SHOW)};
+    let cursor = if let Ok(cursor) = cursor(IDCursor::Arrow){
+        cursor
+    } 
+    else {
+        panic!("Cursor creation failed");
+    };
+    let hwnd = create_window(name, cursor, window_procedure);
     let mut msg = Msg::default();
     loop{
-        let message = unsafe{GetMessageW(&mut msg, null_mut(), 0, 0)};
-        if message == 0{
+        let is_done = update(&mut msg);
+        if is_done{
             break;
         }
-        else if message == -1{
-            panic!();
-        }
-        else {
-            unsafe{TranslateMessage(&msg); 
-            DispatchMessageW(&msg);}
-        }
     }
+}
+
+//Custom window procedure.
+//Can customize behaviour.
+unsafe extern "system" fn window_procedure(hWnd: HWnd, Msg: UInt, wParam: WParam, lParam: LParam,) -> LResult{
+    match Msg{
+        WM_NCCREATE => {
+            let createstruct: *mut CreateStructW = lParam as *mut _;
+            if createstruct.is_null() {
+                return 0;
+            }
+            let boxed_i32_ptr: *mut i32 = (*createstruct).lpCreateParams().cast();
+            SetWindowLongPtrW(hWnd, GWLP_USERDATA, boxed_i32_ptr as LongPtr);
+            return 1;
+        }
+        WM_CLOSE => {let message = wide_str("Do you really want to exit?");
+            let title = wide_str("Quit");
+            let result = MessageBoxW(hWnd, message.as_ptr(), title.as_ptr(), MB_OKCANCEL);
+            if result == IDOK{
+            DestroyWindow(hWnd);
+            }
+            else {
+                return 0;
+            }
+        },
+        WM_DESTROY => {
+            let ptr = GetWindowLongPtrW(hWnd, GWLP_USERDATA) as *mut i32;
+            drop(Box::from_raw(ptr));
+            PostQuitMessage(0);
+        },
+        WM_PAINT => {
+            let mut ps = PaintStruct::default();
+            let hdc = BeginPaint(hWnd, &mut ps);
+            FillRect(hdc, ps.rcPaint(), (COLOR_WINDOW +5) as HBrush);
+            EndPaint(hWnd, &ps);
+          }
+         _ => return DefWindowProcW(hWnd, Msg, wParam, lParam),
+        }
+        0
 }
