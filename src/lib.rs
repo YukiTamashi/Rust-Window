d117b74f4318e0ef0d31faec3f5d6a9fdc11a917
Yukinori <38091245+YukiTamashi@users.noreply.github.com>
2022-12-13 22:49:09 -0300

    started replacing with windows crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,77 +1,9 @@
+
+
 #[cfg(windows)]
 pub mod win32 {
+    use windows::Win32::{UI::WindowsAndMessaging::*, System::LibraryLoader::*, Foundation::*, Graphics::Gdi::*};
 
-    use std::ffi::c_void;
-    use std::ptr::null_mut;
-
-    macro_rules! unsafe_impl_default_zeroed {
-        ($t:ty) => {
-            impl Default for $t {
-                #[inline]
-                #[must_use]
-                fn default() -> Self {
-                    unsafe { core::mem::zeroed() }
-                }
-            }
-        };
-    }
-
-    const SW_SHOW: CInt = 5;
-    const WS_OVERLAPPED: u32 = 0x00000000;
-    const WS_CAPTION: u32 = 0x00C00000;
-    const WS_SYSMENU: u32 = 0x00080000;
-    const WS_THICKFRAME: u32 = 0x00040000;
-    const WS_MINIMIZEBOX: u32 = 0x00020000;
-    const WS_MAXIMIZEBOX: u32 = 0x00010000;
-    const WS_OVERLAPPEDWINDOW: u32 =
-        WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX;
-    const CW_USEDEFAULT: CInt = 0x80000000_u32 as CInt;
-    const IDC_ARROW: LPCWStr = MakeIntResourceW(32512);
-    const WM_CLOSE: u32 = 0x0010;
-    const WM_DESTROY: u32 = 0x0002;
-    const WM_CREATE: u32 = 0x0001;
-    const WM_NCCREATE: u32 = 0x0081;
-    const WM_PAINT: u32 = 0x000F;
-    const COLOR_WINDOW: u32 = 5;
-    const MB_OKCANCEL: u32 = 0x00000001;
-    const IDOK: i32 = 1;
-    const GWLP_USERDATA: i32 = -21;
-    const WM_SETCURSOR: u32 = 0x0020;
-
-    type Atom = Word;
-    type Bool = CInt;
-    type CInt = i32;
-    type CUInt = u32;
-    type DWord = u32;
-    type Handle = *mut c_void;
-    type HBrush = Handle;
-    type HCursor = HIcon;
-    type HDc = Handle;
-    type HIcon = Handle;
-    type HInstance = Handle;
-    type HMenu = Handle;
-    type HModule = HInstance;
-    type HWnd = Handle;
-    type Int = CInt;
-    type Long = i32;
-    type LongPtr = isize;
-    type LParam = LongPtr;
-    type LPMsg = *mut Msg;
-    type LPVoid = *mut c_void;
-    type LPWStr = *mut WChar;
-    type LPCWStr = *const WCharT;
-    type LResult = LongPtr;
-    type UInt = CUInt;
-    type UIntPtr = usize;
-    type ULongPtr = usize;
-    type WCharT = u16;
-    type WChar = WCharT;
-    type WParam = UIntPtr;
-    type Word = u16;
-    type WNDProc = Option<
-        fn(hwnd: HWnd, Msg: UInt, wParam: WParam, lParam: LParam) -> LResult,
-    >;
-    
     #[derive(Debug)]
     pub struct CursorError;
 
@@ -105,135 +37,6 @@ pub mod win32 {
         UpArrow = 32516,
         /// Hourglass
         Wait = 32514,
-    }
-    #[repr(C)]
-    struct WNDClassW {
-        style: UInt,
-        lpfnWndProc: WNDProc,
-        cbClsExtra: Int,
-        cbWndExtra: Int,
-        hInstance: HInstance,
-        hIcon: HIcon,
-        hCursor: HCursor,
-        hbrBackground: HBrush,
-        lpszMenuName: LPCWStr,
-        lpszClassName: LPCWStr,
-    }
-
-    #[repr(C)]
-    pub struct Msg {
-        hwnd: HWnd,
-        message: UInt,
-        wParam: WParam,
-        lParam: LParam,
-        time: DWord,
-        pt: Point,
-        lPrivate: DWord,
-    }
-
-    #[repr(C)]
-    struct Point {
-        x: Long,
-        y: Long,
-    }
-
-    #[repr(C)]
-    struct PaintStruct {
-        hdc: HDc,
-        fErase: Bool,
-        rcPaint: Rect,
-        fRestore: Bool,
-        fIncUpdate: Bool,
-        rgbReserved: [u8; 32],
-    }
-
-    impl PaintStruct {
-        pub fn rcPaint(&self) -> &Rect {
-            &self.rcPaint
-        }
-    }
-    #[repr(C)]
-    struct Rect {
-        left: i32,
-        top: i32,
-        right: i32,
-        bottom: i32,
-    }
-
-    #[repr(C)]
-    struct CreateStructW {
-        lpCreateParams: LPVoid,
-        hInstance: HInstance,
-        hMenu: HMenu,
-        hwndParent: HWnd,
-        cy: CInt,
-        cx: CInt,
-        y: CInt,
-        x: CInt,
-        style: Long,
-        lpszName: LPCWStr,
-        lpszClass: LPCWStr,
-        dwExStyle: DWord,
-    }
-
-    impl CreateStructW {
-        pub fn lpCreateParams(&self) -> LPVoid {
-            self.lpCreateParams
-        }
-    }
-
-    unsafe_impl_default_zeroed!(WNDClassW);
-    unsafe_impl_default_zeroed!(Msg);
-    unsafe_impl_default_zeroed!(Rect);
-    unsafe_impl_default_zeroed!(PaintStruct);
-
-    #[link(name = "Kernel32")]
-    extern "system" {
-        fn GetModuleHandleW(lpModuleName: LPCWStr) -> HModule;
-        fn GetLastError() -> DWord;
-        fn CreateWindowExW(
-            dwExStyle: DWord,
-            lpClassName: LPCWStr,
-            lpWindowName: LPCWStr,
-            dwStyle: DWord,
-            X: Int,
-            Y: Int,
-            nWidth: Int,
-            nHeight: Int,
-            hWndParent: HWnd,
-            hMenu: HMenu,
-            h_instance: HInstance,
-            lpParam: LPVoid,
-        ) -> HWnd;
-    }
-
-    #[link(name = "User32")]
-    extern "system" {
-        fn ShowWindow(hWnd: HWnd, nCmdShow: Int) -> Bool;
-        fn RegisterClassW(lpWndClass: *const WNDClassW) -> Atom;
-        fn DefWindowProcW(hWnd: HWnd, Msg: UInt, wParam: WParam, lParam: LParam) -> LResult;
-        fn GetMessageW(lpMsg: LPMsg, hWnd: HWnd, wMsgFilterMin: UInt, wMsgFilterMax: UInt) -> Bool;
-        fn TranslateMessage(lpMsg: *const Msg) -> Bool;
-        fn DispatchMessageW(lpMsg: *const Msg) -> LResult;
-        fn DestroyWindow(hWnd: HWnd) -> Bool;
-        fn PostQuitMessage(nExitCode: CInt);
-        fn LoadCursorW(h_instance: HInstance, lpCursorName: LPCWStr) -> HCursor;
-        fn BeginPaint(hWnd: HWnd, lpPaint: *mut PaintStruct) -> HDc;
-        fn EndPaint(hWnd: HWnd, lpPaint: *const PaintStruct) -> Bool;
-        fn FillRect(hDC: HDc, lprc: *const Rect, hbr: HBrush) -> Int;
-        fn MessageBoxW(hWnd: HWnd, lpText: LPCWStr, lpCaption: LPCWStr, uType: UInt) -> CInt;
-        fn SetWindowLongPtrW(hWnd: HWnd, nIndex: Int, dwNewLong: LongPtr) -> LongPtr;
-        fn GetWindowLongPtrW(hWnd: HWnd, nIndex: CInt) -> LongPtr;
-        fn SetCursor(hCursor: HCursor) -> HCursor;
-        fn SetWindowTextW(hWnd: HWnd, lpString: LPCWStr) -> Bool;
-    }
-
-    fn get_handle() -> HModule {
-        unsafe { GetModuleHandleW(std::ptr::null()) }
-    }
-
-    const fn MakeIntResourceW(i: Word) -> LPWStr {
-        i as LPWStr
     }
 
     fn register_window(
@@ -343,9 +146,9 @@ pub mod win32 {
                 return 1;
             }
             WM_CLOSE => {
-                let message = wide_str("Do you really want to exit?");
-                let title = wide_str("Quit");
-                let result = MessageBoxW(hWnd, message.as_ptr(), title.as_ptr(), MB_OKCANCEL);
+                let message = "Do you really want to exit?";
+                let title = "Quit";
+                let result = MessageBoxW(hWnd, message.into(), title.into(), MB_OKCANCEL);
                 if result == IDOK {
                     DestroyWindow(hWnd);
                 } else {
@@ -358,9 +161,9 @@ pub mod win32 {
                 PostQuitMessage(0);
             }
             WM_PAINT => {
-                let mut ps = PaintStruct::default();
+                let mut ps = PAINTSTRUCT::default();
                 let hdc = BeginPaint(hWnd, &mut ps);
-                FillRect(hdc, ps.rcPaint(), (COLOR_WINDOW + 5) as HBrush);
+                FillRect(hdc, &ps.rcPaint, (COLOR_WINDOW.into() + 5) as HBRUSH);
                 EndPaint(hWnd, &ps);
             }
             _ => return DefWindowProcW(hWnd, Msg, wParam, lParam),
